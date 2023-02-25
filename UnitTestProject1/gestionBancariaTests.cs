6f4207d304c78f8815ac0c57a276ee1d8b8c18c5
ZPN1DAM <zdrplanik@alu.edu.gva.es>
2023-02-25 09:00:59 +0100

    Metodo reintegro hecho y probado

--- UnitTestProject1/gestionBancariaTests.cs
+++ UnitTestProject1/gestionBancariaTests.cs
@@ -8,21 +8,43 @@ namespace UnitTestProject1
     public class gestionBancariaTests
     {
         [TestMethod]
-      
+
         // unit test code [TestMethod]
-            public void validarMetodoIngreso()
-            {
-                // preparación del caso de prueba
-                double saldoInicial = 1000;
-                double ingreso = 500;
-                double saldoActual = 0;
-                double saldoEsperado = 1500;
-                gestionBancaria cuenta = new gestionBancaria(saldoInicial);
-                // Método a probar
-                cuenta.realizarIngreso(ingreso);
-                // afirmación de la prueba (valor esperado Vs. Valor obtenido)
-                saldoActual = cuenta.obtenerSaldo();
+        public void validarMetodoIngreso()
+        {
+            // preparación del caso de prueba
+            double saldoInicial = 1000;
+            double ingreso = 500;
+            double saldoActual = 0;
+            double saldoEsperado = 1500;
+            gestionBancaria cuenta = new gestionBancaria(saldoInicial);
+            // Método a probar
+            cuenta.realizarIngreso(ingreso);
+            // afirmación de la prueba (valor esperado Vs. Valor obtenido)
+            saldoActual = cuenta.obtenerSaldo();
             Assert.AreEqual(saldoEsperado, saldoActual, 0.001, "El saldo de la cuenta no es correcto");
         }
+        [TestMethod]
+        //[ExpectedException(typeof(Exception))]
+        public void validarMetodoReintrego()
+        {
+            //preparamos los casos de prueba
+            double saldo = 1000;
+            double retiro = 500;
+            double saldoActual = 0;
+            double saldoEsperado = 500;
+
+            gestionBancaria cuenta = new gestionBancaria(saldo);
+
+            //metodo a probar
+            cuenta.realizarReintegro(retiro);
+
+            //valor esperado vs valor obtenido
+            saldoActual = cuenta.obtenerSaldo();
+
+            Assert.AreEqual(saldoEsperado, saldoActual, 0.001, "El saldo de la cuenta no es correcto");
         }
-}
+    }
+} 
+
+
