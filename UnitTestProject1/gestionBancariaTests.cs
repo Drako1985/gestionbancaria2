426b28df14703cd7da7eeb7ea59da6f5e004f057
ZPN1DAM <zdrplanik@alu.edu.gva.es>
2023-02-25 08:49:28 +0100

    error arreglado

--- UnitTestProject1/gestionBancariaTests.cs
+++ UnitTestProject1/gestionBancariaTests.cs
@@ -1,14 +1,28 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using gestionBancariaApp;
 
 namespace UnitTestProject1
 {
     [TestClass]
-    public class UnitTest1
+    public class gestionBancariaTests
     {
         [TestMethod]
-        public void TestMethod1()
-        {
+      
+        // unit test code [TestMethod]
+            public void validarMetodoIngreso()
+            {
+                // preparación del caso de prueba
+                double saldoInicial = 1000;
+                double ingreso = 500;
+                double saldoActual = 0;
+                double saldoEsperado = 1500;
+                gestionBancaria cuenta = new gestionBancaria(saldoInicial);
+                // Método a probar
+                cuenta.realizarIngreso(ingreso);
+                // afirmación de la prueba (valor esperado Vs. Valor obtenido)
+                saldoActual = cuenta.obtenerSaldo();
+            Assert.AreEqual(saldoEsperado, saldoActual, 0.001, "El saldo de la cuenta no es correcto");
         }
-    }
+        }
 }
